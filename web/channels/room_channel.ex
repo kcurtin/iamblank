8683ce6e241a4e0fb8f16ef6fd59cc8eb926308f
Kevin Curtin <kevincurtin88@gmail.com>
2016-03-20 17:30:32 -0400

    Working a bit

--- web/channels/room_channel.ex
+++ web/channels/room_channel.ex
@@ -1,21 +1,45 @@
 defmodule Iamblank.RoomChannel do
   use Phoenix.Channel
 
+  # intercept ["join_room"]
+
   def join("rooms:lobby", _message, socket) do
     {:ok, socket}
   end
 
-  def join("rooms:" <> _private_room_id, _params, _socket) do
-    {:error, %{reason: "unauthorized"}}
+  def join("rooms:" <> _private_room_id, _params, socket) do
+    # TODO: Find room based on _private_room_id
+
+    comments = [
+      %{id: 1, author: "Kevin", body: "This is a great comment"},
+      %{id: 2, author: "Lauren", body: "Wow, what a good reply"}
+    ]
+    reply = %{comments: comments}
+    socket = assign(socket, :comments, comments)
+    {:ok, reply, socket}
   end
 
-  def handle_in("new_msg", %{"body" => body}, socket) do
-    broadcast! socket, "new_msg", %{body: body}
+  def handle_in("join_room", %{"body" => body}, socket) do
+    broadcast! socket, "join_room", %{body: body}
     {:noreply, socket}
   end
 
-  def handle_out("new_msg", payload, socket) do
-    push socket, "new_msg", payload
+  def handle_out("join_room", payload, socket) do
+    push socket, "join_room", payload
+    {:noreply, socket}
+  end
+
+  def handle_in("new_comment", %{"comment" => comment}, socket) do
+    new_comment = %{id: 3, author: comment["author"], body: comment["body"]}
+    comments = socket.assigns.comments
+    new_comments = List.insert_at(comments, -1, new_comment)
+    socket = assign(socket, :comments, new_comments)
+    broadcast! socket, "new_comment", %{comments: new_comments}
+    {:noreply, socket}
+  end
+
+  def handle_out("new_comment", payload, socket) do
+    push socket, "new_comment", payload
     {:noreply, socket}
   end
 end
--- web/static/js/app.js
+++ web/static/js/app.js
@@ -25,16 +25,11 @@ let socket = new Socket("/socket", {params: {token: window.userToken}})
 
 socket.connect()
 
-let channel = socket.channel("rooms:lobby", {})
+let lobbyChannel = socket.channel("rooms:lobby", {})
 
-channel.join()
-  .receive("ok", resp => { console.log("Joined successfully", resp) })
-  .receive("error", resp => { console.log("Unable to join", resp) })
-
-
-channel.on("new_msg", payload => {
-  console.log(`${payload.body}`)
-})
+lobbyChannel.join()
+  .receive("ok", resp => { console.log("Joined lobby", resp) })
+  .receive("error", resp => { console.log("Unable to join lobby", resp) })
 
 import React from "react"
 import ReactDOM from "react-dom"
@@ -48,20 +43,142 @@ var IAmBlank = React.createClass({
   },
   handleTextChange: function(e) {
     this.setState({text: e.target.value})
-    channel.push("new_msg", {body: this.state.text})
   },
-  render() {
+  handleSubmit: function(e) {
+    e.preventDefault();
+    var text = this.state.text.trim();
+    if (!text) { return; }
+    lobbyChannel.push("join_room", {body: text})
+    this.setState({text: ''});
+  },
+  render: function() {
     return (
-      <input
-        type="text"
-        value={this.state.text}
-        onChange={this.handleTextChange}
-      />
-    )
+      <form className="commentForm" onSubmit={this.handleSubmit}>
+        <input
+          type="text"
+          placeholder="Your name"
+          value={this.state.text}
+          onChange={this.handleTextChange}
+        />
+      </form>
+    );
   }
 })
+
+var CommentBox = React.createClass({
+  handleCommentSubmit: function(comment) {
+    this.state.channel.push("new_comment", {comment: comment})
+  },
+  getInitialState: function() {
+    return {data: [], channel: {}};
+  },
+  componentDidMount: function() {
+    // lobbyChannel.leave()
+    // console.log(`${payload.body}`)
+    var roomChannel = socket.channel(`rooms:${this.props.room}`, {})
+    this.setState({channel: roomChannel})
+    roomChannel.join()
+    .receive("ok", resp => {
+      this.setState({data: resp.comments}),
+      this.state.channel.on("new_comment", payload => {
+        this.setState({data: payload.comments})
+      })
+    })
+    .receive("error", resp => {
+      console.log("Unable to join room yo", resp)
+    })
+  },
+  render: function() {
+    return (
+      <div className="commentBox">
+        <h1>Comments</h1>
+        <CommentList data={this.state.data} />
+        <CommentForm onCommentSubmit={this.handleCommentSubmit} />
+      </div>
+    );
+  }
+});
+
+var CommentList = React.createClass({
+  render: function() {
+    var commentNodes = this.props.data.map(function(comment) {
+      return (
+        <Comment author={comment.author}>
+          {comment.body}
+        </Comment>
+      );
+    });
+    return (
+      <div className="commentList">
+        {commentNodes}
+      </div>
+    );
+  }
+});
+
+var CommentForm = React.createClass({
+  getInitialState: function() {
+    return {author: '', body: ''};
+  },
+  handleAuthorChange: function(e) {
+    this.setState({author: e.target.value});
+  },
+  handleBodyChange: function(e) {
+    this.setState({body: e.target.value});
+  },
+  handleSubmit: function(e) {
+    e.preventDefault();
+    var author = this.state.author.trim();
+    var body = this.state.body.trim();
+    if (!body || !author) {
+      return;
+    }
+    this.props.onCommentSubmit({author: author, body: body});
+    this.setState({author: '', body: ''});
+  },
+  render: function() {
+    return (
+      <form className="commentForm" onSubmit={this.handleSubmit}>
+        <input
+          type="text"
+          placeholder="Your name"
+          value={this.state.author}
+          onChange={this.handleAuthorChange}
+        />
+        <input
+          type="text"
+          placeholder="Say something...."
+          value={this.state.body}
+          onChange={this.handleBodyChange}
+        />
+        <input type="submit" value="Post" />
+      </form>
+    );
+  }
+});
+
+var Comment = React.createClass({
+  render: function() {
+    return (
+      <div className="comment">
+        <h2 className="commentAuthor">
+          {this.props.author}
+        </h2>
+        {this.props.children.toString()}
+      </div>
+    );
+  }
+});
 
 ReactDOM.render(
   <IAmBlank/>,
   document.getElementById("i-am-blank")
 )
+
+lobbyChannel.on("join_room", payload => {
+  ReactDOM.render(
+    <CommentBox lobbyChannel={lobbyChannel} room={payload.body} />,
+    document.getElementById('content')
+  );
+})
+
